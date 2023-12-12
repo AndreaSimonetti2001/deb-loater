349a2c3534f614100e63bbc7db776f3fbcc8b1e9
Andrea Simonetti <119041441+AndreaSimonetti2001@users.noreply.github.com>
2023-12-12 01:33:12 +0100

    Update debloater.sh

--- debloater.sh
+++ debloater.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get remove goldendict gnome-robots gnome-chess aisleriot five-or-more gnome-mahjongg tali four-in-a-row gnome-klotski gnome-mines gnome-nibbles gnome-2048 gnome-sudoku gnome-taquin gnome-tetravex hitori lightsoff quadrapassel swell-foop iagno anthy kasumi -y
+sudo apt remove goldendict gnome-robots gnome-chess aisleriot five-or-more gnome-mahjongg tali four-in-a-row gnome-klotski gnome-mines gnome-nibbles gnome-2048 gnome-sudoku gnome-taquin gnome-tetravex hitori lightsoff quadrapassel swell-foop iagno anthy kasumi -y
 killall -9 uim
-sudo apt-get remove uim uim-data uim-mozc uim-xim im-config mozc-data mozc-utils-gui fcitx5-data fcitx-bin fcitx-module-dbus fcitx-config-common hunspell aspell eject vino xterm mlterm-common mlterm mlterm-tiny xiterm+thai malcontent -y
+sudo apt remove uim uim-data uim-mozc uim-xim im-config mozc-data mozc-utils-gui fcitx5-data fcitx-bin fcitx-module-dbus fcitx-config-common hunspell aspell eject vino xterm mlterm-common mlterm mlterm-tiny xiterm+thai malcontent -y
 sudo apt autoremove

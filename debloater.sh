eff3c934108b925366ba240e6ea999678354e99c
Andrea Simonetti <119041441+AndreaSimonetti2001@users.noreply.github.com>
2023-12-12 01:58:44 +0100

    Update debloater.sh

--- debloater.sh
+++ debloater.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get remove -y --ignore-missing goldendict gnome-robots gnome-chess aisleriot five-or-more gnome-mahjongg tali four-in-a-row gnome-klotski gnome-mines gnome-nibbles gnome-2048 gnome-sudoku gnome-taquin gnome-tetravex hitori lightsoff quadrapassel swell-foop iagno anthy kasumi uim uim-data uim-mozc uim-xim im-config mozc-data mozc-utils-gui fcitx5-data fcitx-bin fcitx-module-dbus fcitx-config-common hunspell aspell eject vino xterm mlterm-common mlterm mlterm-tiny xiterm+thai malcontent
-sudo apt autoremove
+sudo apt remove -y goldendict gnome-robots gnome-chess aisleriot five-or-more gnome-mahjongg tali four-in-a-row gnome-klotski gnome-mines gnome-nibbles gnome-2048 gnome-sudoku gnome-taquin gnome-tetravex hitori lightsoff quadrapassel swell-foop iagno anthy kasumi uim uim-data uim-mozc uim-xim im-config mozc-data mozc-utils-gui fcitx5-data fcitx-bin fcitx-module-dbus fcitx-config-common hunspell aspell eject vino xterm mlterm-common mlterm mlterm-tiny xiterm+thai malcontent
+sudo apt autoremove -y
